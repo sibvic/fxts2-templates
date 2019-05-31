58db2a814d44cac1e4dd94065dc355003e90239c
Victor Tereschenko <sibvic@gmail.com>
2019-05-31 11:38:55 +0600

    Signaler + readme

---
+++ snippets/readme.md
@@ -0,0 +1,9 @@
+# Snippets
+
+## signaler_indi
+
+Shows signals of the indicator on the chart and sends an alert when running live.
+
+## Signaler
+
+Sends an alert in the next ways: message dialog, popup (indicators only), sound alert, email, log, external receiver (Telegram/Discord/other trading platforms), DDE (like Excel)
---
+++ snippets/signaler.lua
@@ -0,0 +1,256 @@
+signaler = {};
+signaler.Name = "Signaler";
+signaler.Debug = false;
+signaler.Version = "1.5";
+
+signaler._show_alert = nil;
+signaler._sound_file = nil;
+signaler._recurrent_sound = nil;
+signaler._email = nil;
+signaler._ids_start = nil;
+signaler._advanced_alert_timer = nil;
+signaler._tz = nil;
+signaler._alerts = {};
+
+function signaler:trace(str) if not self.Debug then return; end core.host:trace(self.Name .. ": " .. str); end
+function signaler:OnNewModule(module) end
+function signaler:RegisterModule(modules) for _, module in pairs(modules) do self:OnNewModule(module); module:OnNewModule(self); end modules[#modules + 1] = self; self._ids_start = (#modules) * 100; end
+
+function signaler:ToJSON(item)
+    local json = {};
+    function json:AddStr(name, value)
+        local separator = "";
+        if self.str ~= nil then
+            separator = ",";
+        else
+            self.str = "";
+        end
+        self.str = self.str .. string.format("%s\"%s\":\"%s\"", separator, tostring(name), tostring(value));
+    end
+    function json:AddNumber(name, value)
+        local separator = "";
+        if self.str ~= nil then
+            separator = ",";
+        else
+            self.str = "";
+        end
+        self.str = self.str .. string.format("%s\"%s\":%f", separator, tostring(name), value or 0);
+    end
+    function json:AddBool(name, value)
+        local separator = "";
+        if self.str ~= nil then
+            separator = ",";
+        else
+            self.str = "";
+        end
+        self.str = self.str .. string.format("%s\"%s\":%s", separator, tostring(name), value and "true" or "false");
+    end
+    function json:ToString()
+        return "{" .. (self.str or "") .. "}";
+    end
+    
+    local first = true;
+    for idx,t in pairs(item) do
+        local stype = type(t)
+        if stype == "number" then
+            json:AddNumber(idx, t);
+        elseif stype == "string" then
+            json:AddStr(idx, t);
+        elseif stype == "boolean" then
+            json:AddBool(idx, t);
+        elseif stype == "function" or stype == "table" then
+            --do nothing
+        else
+            core.host:trace(tostring(idx) .. " " .. tostring(stype));
+        end
+    end
+    return json:ToString();
+end
+
+function signaler:ArrayToJSON(arr)
+    local str = "[";
+    for i, t in ipairs(self._alerts) do
+        local json = self:ToJSON(t);
+        if str == "[" then
+            str = str .. json;
+        else
+            str = str .. "," .. json;
+        end
+    end
+    return str .. "]";
+end
+
+function signaler:AsyncOperationFinished(cookie, success, message, message1, message2)
+    if cookie == self._advanced_alert_timer and #self._alerts > 0 and (self.last_req == nil or not self.last_req:loading()) then
+        if self._advanced_alert_key == nil then
+            return;
+        end
+
+        local data = self:ArrayToJSON(self._alerts);
+        self._alerts = {};
+        
+        self.last_req = http_lua.createRequest();
+        local query = string.format('{"Key":"%s","StrategyName":"%s","Platform":"FXTS2","Notifications":%s}',
+            self._advanced_alert_key, string.gsub(self.StrategyName or "", '"', '\\"'), data);
+        self.last_req:setRequestHeader("Content-Type", "application/json");
+        self.last_req:setRequestHeader("Content-Length", tostring(string.len(query)));
+
+        self.last_req:start("http://profitrobots.com/api/v1/notification", "POST", query);
+    end
+end
+
+function signaler:FormatEmail(source, period, message)
+    --format email subject
+    local subject = message .. "(" .. source:instrument() .. ")";
+    --format email text
+    local delim = "\013\010";
+    local signalDescr = "Signal: " .. (self.StrategyName or "");
+    local symbolDescr = "Symbol: " .. source:instrument();
+    local messageDescr = "Message: " .. message;
+    local ttime = core.dateToTable(core.host:execute("convertTime", core.TZ_EST, self._ToTime, source:date(period)));
+    local dateDescr = string.format("Time:  %02i/%02i %02i:%02i", ttime.month, ttime.day, ttime.hour, ttime.min);
+    local priceDescr = "Price: " .. source[period];
+    local text = "You have received this message because the following signal alert was received:"
+        .. delim .. signalDescr .. delim .. symbolDescr .. delim .. messageDescr .. delim .. dateDescr .. delim .. priceDescr;
+    return subject, text;
+end
+
+function signaler:Signal(message, source)
+    if source == nil then
+        if instance.source ~= nil then
+            source = instance.source;
+        elseif instance.bid ~= nil then
+            source = instance.bid;
+        else
+            local pane = core.host.Window.CurrentPane;
+            source = pane.Data:getStream(0);
+        end
+    end
+    if self._show_alert then
+        terminal:alertMessage(source:instrument(), source[NOW], message, source:date(NOW));
+    end
+
+    if self._sound_file ~= nil then
+        terminal:alertSound(self._sound_file, self._recurrent_sound);
+    end
+
+    if self._email ~= nil then
+        terminal:alertEmail(self._email, profile:id().. " : " .. message, self:FormatEmail(source, NOW, message));
+    end
+
+    if self._advanced_alert_key ~= nil then
+        self:AlertTelegram(message, source:instrument(), source:barSize());
+    end
+
+    if self._signaler_debug_alert then
+        core.host:trace(message);
+    end
+
+    if self._show_popup then
+        local subject, text = self:FormatEmail(source, NOW, message);
+        core.host:execute("prompt", self._ids_start + 2, subject, text);
+    end
+
+    if self._dde_alerts then
+        dde_server:set(self.dde_topic, self.dde_alerts, message);
+    end
+end
+
+function signaler:AlertTelegram(message, instrument, timeframe)
+    if core.host.Trading:getTradingProperty("isSimulation") then
+        return;
+    end
+    local alert = {};
+    alert.Text = message or "";
+    alert.Instrument = instrument or "";
+    alert.TimeFrame = timeframe or "";
+    self._alerts[#self._alerts + 1] = alert;
+end
+
+function signaler:Init(parameters)
+    parameters:addInteger("signaler_ToTime", "Convert the date to", "", 6)
+    parameters:addIntegerAlternative("signaler_ToTime", "EST", "", 1)
+    parameters:addIntegerAlternative("signaler_ToTime", "UTC", "", 2)
+    parameters:addIntegerAlternative("signaler_ToTime", "Local", "", 3)
+    parameters:addIntegerAlternative("signaler_ToTime", "Server", "", 4)
+    parameters:addIntegerAlternative("signaler_ToTime", "Financial", "", 5)
+    parameters:addIntegerAlternative("signaler_ToTime", "Display", "", 6)
+    
+    parameters:addBoolean("signaler_show_alert", "Show Alert", "", true);
+    parameters:addBoolean("signaler_play_sound", "Play Sound", "", false);
+    parameters:addFile("signaler_sound_file", "Sound File", "", "");
+    parameters:setFlag("signaler_sound_file", core.FLAG_SOUND);
+    parameters:addBoolean("signaler_recurrent_sound", "Recurrent Sound", "", true);
+    parameters:addBoolean("signaler_send_email", "Send Email", "", false);
+    parameters:addString("signaler_email", "Email", "", "");
+    parameters:setFlag("signaler_email", core.FLAG_EMAIL);
+    if indicator ~= nil and strategy == nil then
+        parameters:addBoolean("signaler_show_popup", "Show Popup", "", false);
+    end
+    parameters:addBoolean("signaler_debug_alert", "Print Into Log", "", false);
+    parameters:addBoolean("use_advanced_alert", "Send Advanced Alert", "Telegram/Discord/other platform (like MT4)", false)
+	parameters:addString("advanced_alert_key", "Advanced Alert Key",
+		"You can get a key via @profit_robots_bot Telegram Bot. Visit ProfitRobots.com for discord/other platform keys", "")
+    if DDEAlertsSupport then
+        parameters:addBoolean("signaler_dde_export", "DDE Export", "You can export the alert into the Excel or any other application with DDE support (=Service Name|DDE Topic!Alerts)", false);
+        parameters:addString("signaler_dde_service", "Service Name", "The service name must be unique amoung all running instances of the strategy", "TS2ALERTS");
+        parameters:addString("signaler_dde_topic", "DDE Topic", "", "");
+    end
+end
+
+function signaler:Prepare(name_only)
+    self._ToTime = instance.parameters.signaler_ToTime
+    if self._ToTime == 1 then
+        self._ToTime = core.TZ_EST
+    elseif self._ToTime == 2 then
+        self._ToTime = core.TZ_UTC
+    elseif self._ToTime == 3 then
+        self._ToTime = core.TZ_LOCAL
+    elseif self._ToTime == 4 then
+        self._ToTime = core.TZ_SERVER
+    elseif self._ToTime == 5 then
+        self._ToTime = core.TZ_FINANCIAL
+    elseif self._ToTime == 6 then
+        self._ToTime = core.TZ_TS
+    end
+    self._dde_alerts = instance.parameters.signaler_dde_export;
+    if self._dde_alerts then
+        assert(instance.parameters.signaler_dde_topic ~= "", "You need to specify the DDE topic");
+        require("ddeserver_lua");
+        self.dde_server = ddeserver_lua.new(instance.parameters.signaler_dde_service);
+        self.dde_topic = self.dde_server:addTopic(instance.parameters.signaler_dde_topic);
+        self.dde_alerts = self.dde_server:addValue(self.dde_topic, "Alerts");
+    end
+
+    if instance.parameters.signaler_play_sound then
+        self._sound_file = instance.parameters.signaler_sound_file;
+        assert(self._sound_file ~= "", "Sound file must be chosen");
+    end
+    self._show_alert = instance.parameters.signaler_show_alert;
+    self._recurrent_sound = instance.parameters.signaler_recurrent_sound;
+    self._show_popup = instance.parameters.signaler_show_popup;
+    self._signaler_debug_alert = instance.parameters.signaler_debug_alert;
+    if instance.parameters.signaler_send_email then
+        self._email = instance.parameters.signaler_email;
+        assert(self._email ~= "", "E-mail address must be specified");
+    end
+    --do what you usually do in prepare
+    if name_only then
+        return;
+    end
+
+    if instance.parameters.advanced_alert_key ~= "" and instance.parameters.use_advanced_alert then
+        self._advanced_alert_key = instance.parameters.advanced_alert_key;
+        require("http_lua");
+        self._advanced_alert_timer = self._ids_start + 1;
+        core.host:execute("setTimer", self._advanced_alert_timer, 1);
+    end
+end
+
+function signaler:ReleaseInstance()
+    if self.dde_server ~= nil then
+        self.dde_server:close();
+    end
+end
+
+signaler:RegisterModule(Modules);
