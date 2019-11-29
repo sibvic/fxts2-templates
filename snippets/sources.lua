dd4792f3c492574afb860ebdbd8ca77f4e731662
Victor Tereschenko <sibvic@gmail.com>
2019-11-29 13:16:15 +0600

    Custom instrument

--- snippets/sources.lua
+++ snippets/sources.lua
@@ -1,9 +1,9 @@
--- Sources v1.2
+-- Sources v1.3
 local sources = {}
 sources.last_id = 1
 sources.ids = {}
 sources.items = {}
-function sources:Request(id, source, tf, isBid)
+function sources:Request(id, source, tf, isBid, instrument)
 	local ids = {}
 	ids.loading_id = self.last_id
 	ids.loaded_id = self.last_id + 1
@@ -11,11 +11,17 @@ function sources:Request(id, source, tf, isBid)
 	self.last_id = self.last_id + 2
 	self.ids[id] = ids
 
+    if tf == nil then
+        tf = source:barSize()
+    end
 	if isBid == nil then
 		isBid = source:isBid()
-	end
+    end
+    if instrument == nil then
+        instrument = source:instrument();
+    end
 
-	self.items[id] = core.host:execute("getSyncHistory", source:instrument(), tf, isBid, 100, ids.loaded_id, ids.loading_id)
+	self.items[id] = core.host:execute("getSyncHistory", instrument, tf, isBid, 100, ids.loaded_id, ids.loading_id)
 	return self.items[id];
 end
 function sources:AsyncOperationFinished(cookie, successful, message, message1, message2)
