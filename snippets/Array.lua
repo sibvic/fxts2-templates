0b489e38313d6fb2e3a3141d1bfcc1c72e0a5845
Victor Tereschenko <sibvic@gmail.com>
2023-11-23 19:32:00 +0600

    feat: Support arrays of int, float, label and string

--- snippets/Array.lua
+++ snippets/Array.lua
@@ -1,12 +1,37 @@
 Array = {};
-function Array:NewLine()
+function Array:NewArray(size)
     local newArray = {};
     newArray.arr = {};
+    for i = 1, size, 1 do
+        newArray.arr[i] = nil;
+    end
     function newArray:Push(item)
         self.arr[#self.arr + 1] = item;
     end
     function newArray:Get(index)
         return self.arr[index + 1];
     end
+    function newArray:Sum()
+        local sum = 0;
+        for i, v in ipairs(self.arr) do
+            sum = sum + v;
+        end
+        return sum;
+    end
     return newArray;
+end
+function Array:NewLine(size)
+    return Array:NewArray(size);
+end
+function Array:NewInt(size)
+    return Array:NewArray(size);
+end
+function Array:NewFloat(size)
+    return Array:NewArray(size);
+end
+function Array:NewLabel(size)
+    return Array:NewArray(size);
+end
+function Array:NewString(size)
+    return Array:NewArray(size);
 end
