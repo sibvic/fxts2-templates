40591500dba3f1989aaa2cbb3712c6a62697971f
Victor Tereschenko <sibvic@gmail.com>
2023-11-28 16:10:15 +0600

    feat: array.min/max, shift/unshift, slice

--- snippets/Array.lua
+++ snippets/Array.lua
@@ -1,22 +1,73 @@
 Array = {};
+function Array:Max(array)
+    local maxVal = array:Get(0);
+    for i = 1, array:Size() - 1 do
+        local val = array:Get(i);
+        if maxVal == nil or maxVal < val then
+            maxVal = val;
+        end
+    end
+    return maxVal;
+end
+function Array:Min(array)
+    local minVal = array:Get(0);
+    for i = 1, array:Size() - 1 do
+        local val = array:Get(i);
+        if minVal == nil or minVal > val then
+            minVal = val;
+        end
+    end
+    return minVal;
+end
 function Array:NewArray(size)
     local newArray = {};
     newArray.arr = {};
     for i = 1, size, 1 do
         newArray.arr[i] = nil;
     end
-    function newArray:Push(item)
-        self.arr[#self.arr + 1] = item;
-    end
-    function newArray:Get(index)
-        return self.arr[index + 1];
-    end
+    function newArray:Push(item) self.arr[#self.arr + 1] = item; end
+    function newArray:Get(index) return self.arr[index + 1]; end
+    function newArray:Max() return Array:Max(self); end
+    function newArray:Min() return Array:Min(self); end
+    function newArray:Size() return #self.arr; end
     function newArray:Sum()
         local sum = 0;
         for i, v in ipairs(self.arr) do
             sum = sum + v;
         end
         return sum;
+    end
+    function newArray:Unshift(value)
+        local nextValue = value;
+        for i = 1, #self.arr, 1 do
+            local current = self.arr[i];
+            self.arr[i] = nextValue;
+            nextValue = current;
+        end
+        self.arr[#self.arr + 1] = nextValue;
+    end
+    function newArray:Shift(value)
+        table.remove(self.arr, 1);
+        self.arr[#self.arr + 1] = nextValue;
+    end
+    function newArray:Slice(from, to)
+        local slice = {};
+        slice.Parent = self;
+        slice.From = from;
+        slice.To = to;
+        function slice:Get(index)
+            return self.Parent:Get(index + self.From);
+        end
+        function slice:Size()
+            return self.To - self.From;
+        end
+        function slice:Max()
+            return Array:Max(self);
+        end
+        function slice:Min()
+            return Array:Min(self);
+        end
+        return slice;
     end
     return newArray;
 end
