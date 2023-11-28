e59262f6c07e9d45a50e3d798496d1a049617812
Victor Tereschenko <sibvic@gmail.com>
2023-11-28 16:10:47 +0600

    feat: Referencing to old boxes and Delete func

--- snippets/Box.lua
+++ snippets/Box.lua
@@ -1,28 +1,36 @@
 Box = {};
 Box.AllBoxs = {};
+Box.AllSeries = {};
 function Box:Clear()
     Box.AllBoxs = {};
+    Box.AllSeries = {};
 end
-function Box:New(id, left, top, right, bottom)
+function Box:New(id, seriesId, left, top, right, bottom)
     local newBox = {};
+    newBox.SeriesId = seriesId;
     newBox.Left = left;
     newBox.Top = top;
     newBox.Right = right;
     newBox.Bottom = bottom;
-    newBox.BorderColor = core.colors().Blue;
-    newBox.BgColor = core.colors().Blue;
     newBox.BorderWidth = 1;
     newBox.BorderStyle = core.LINE_SOLID;
-    function newBox:SetBGColor(clr)
+    newBox.BgColor = core.colors().Blue;
+    function newBox:SetBgColor(clr)
         self.BgColorTransparency = (math.floor(clr / 16777216) % 256);
         self.BgColor = clr - self.BgColorTransparency * 16777216;
         self.BrushId = nil;
+        return self;
     end
+    newBox.BorderColor = core.colors().Blue;
     function newBox:SetBorderColor(clr)
         self.BorderColor = clr;
         self.PenId = nil;
+        return self;
     end
     function newBox:Draw(stage, context)
+        if self.Top == nil or self.Left == nil or self.Bottom == nil or self.Right == nil then
+            return;
+        end
         if self.PenId == nil then
             self.PenId = Graphics:FindPen(self.BorderWidth, self.BorderColor, self.BorderStyle, context);
         end
@@ -36,8 +44,38 @@ function Box:New(id, left, top, right, bottom)
         context:drawRectangle(self.PenId, self.BrushId, x1, y1, x2, y2, self.BgColorTransparency)
         context:drawRectangle(self.PenId, -1, x1, y1, x2, y2)
     end
-    self.AllBoxs[id] = newBox;
+    function newBox:Get(index)
+        return Box.AllSeries[self.SeriesId][index + 1];
+    end
+    self.AllBoxs[id .. "_" .. seriesId] = newBox;
+    if self.AllSeries[seriesId] == nil then
+        self.AllSeries[seriesId] = {};
+    end
+    table.insert(self.AllSeries[seriesId], 1, newBox);
     return newBox;
+end
+function Box:Delete(box)
+    if box == nil then
+        return;
+    end
+    self:removeFromAllBoxes(box);
+    self:removeFromSeries(box);
+end
+function Box:removeFromSeries(box)
+    for i = 1, #self.AllSeries[box.SeriesId] do
+        if self.AllSeries[box.SeriesId][i] == box then
+            table.remove(self.AllSeries[box.SeriesId], i);
+            return;
+        end
+    end
+end
+function Box:removeFromAllBoxes(box)
+    for i = 1, #self.AllBoxs do
+        if self.AllBoxs[i] == box then
+            table.remove(self.AllBoxs, i);
+            return;
+        end
+    end
 end
 function Box:Draw(stage, context)
     if stage ~= 2 then
