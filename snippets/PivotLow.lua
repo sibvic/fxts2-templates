e973a4c33e89f53af30a95940fad030c41c5041c
Victor Tereschenko <sibvic@gmail.com>
2023-12-04 19:22:15 +0600

    feat: Support pivot min/max

---
+++ snippets/PivotLow.lua
@@ -0,0 +1,24 @@
+function CreatePivotLow(source, leftbars, rightbars)
+    local pivot = {};
+    pivot.Source = source;
+    pivot.LeftBars = leftbars;
+    pivot.RightBars = rightbars;
+    function pivot:get(period)
+        if period - self.RightBars - self.LeftBars - 1 < 0 or not self.Source:hasData(period - self.RightBars) then
+            return nil;
+        end
+        local ref = self.Source:tick(period - self.RightBars);
+        for i = period - self.RightBars - self.LeftBars, period - self.RightBars - 1 do
+            if not self.Source:hasData(i) or self.Source:tick(i) <= ref then
+                return nil;
+            end
+        end
+        for i = period - self.LeftBars + 1, period do
+            if not self.Source:hasData(i) or self.Source:tick(i) <= ref then
+                return nil;
+            end
+        end
+        return ref;
+    end
+    return pivot;
+end
