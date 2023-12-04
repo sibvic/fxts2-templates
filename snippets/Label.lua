a36b38e94239fdeb23986a34f146e9896f75e42e
Victor Tereschenko <sibvic@gmail.com>
2023-12-04 19:21:42 +0600

    feat: extended support of labels

--- snippets/Label.lua
+++ snippets/Label.lua
@@ -3,30 +3,93 @@ Label.AllLabels = {};
 function Label:Clear()
     Label.AllLabels = {};
 end
+function Label:SetText(label, text)
+    if label == nil then
+        return;
+    end
+    label:SetText(text);
+end
+function Label:SetX(label, x)
+    if label == nil then
+        return;
+    end
+    label:SetX(x);
+end
+function Label:SetY(label, y)
+    if label == nil then
+        return;
+    end
+    label:SetY(y);
+end
+function Label:GetX(label)
+    if label == nil then
+        return;
+    end
+    return label:GetX();
+end
+function Label:GetY(label)
+    if label == nil then
+        return;
+    end
+    return label:GetY();
+end
 function Label:New(id, period, price)
     local newLabel = {};
     newLabel.X = period;
+    function newLabel:SetX(x)
+        self.X = x;
+        return self;
+    end
+    function newLabel:GetX()
+        return self.X;
+    end
     newLabel.Y = price;
+    function newLabel:SetY(y)
+        self.Y = y;
+        return self;
+    end
+    function newLabel:GetY()
+        return self.Y;
+    end
     newLabel.Text = "";
-    newLabel.BGColor = core.colors().Black;
-    newLabel.TextColor = core.colors().Red;
     function newLabel:SetText(text)
         self.Text = text;
         return self;
     end
+    newLabel.BGColor = nil;
     function newLabel:SetColor(clr)
+        self.BgColorTransparency = (math.floor(clr / 16777216) % 256);
         self.BGColor = clr;
+        self.BGPenId = nil;
+        self.BGBrushId = nil;
         return self;
     end
+    newLabel.TextColor = core.colors().Black;
     function newLabel:SetTextColor(clr)
         self.TextColor = clr;
         return self;
     end
     function newLabel:Draw(stage, context)
+        if self.X == nil or self.Y == nil then
+            return;
+        end
         local W, H = context:measureText(Label.FontId, self.Text, context.LEFT);
         visible, y = context:pointOfPrice(self.Y);
         x1, x = context:positionOfBar(self.X)
-        context:drawText(Label.FontId, self.Text, self.TextColor, -1, x - W / 2, y - H / 2, x + W /2, y + H / 2, 0);
+        x_from = x - W / 2;
+        y_from = y - H / 2;
+        x_to = x + W / 2;
+        y_to = y + H / 2;
+        if self.BGColor ~= nil then
+            if self.BGPenId == nil then
+                self.BGPenId = Graphics:FindPen(1, self.BGColor, core.LINE_SOLID, context);
+            end
+            if self.BGBrushId == nil then
+                self.BGBrushId = Graphics:FindBrush(self.BGColor, context);
+            end
+            context:drawRectangle(self.BGPenId, self.BGBrushId, x_from - 1, y_from - 1, x_to + 1, y_to + 1, self.BgColorTransparency)
+        end
+        context:drawText(Label.FontId, self.Text, self.TextColor, -1, x_from, y_from, x_to, y_to, 0);
     end
     self.AllLabels[id] = newLabel;
     return newLabel;
