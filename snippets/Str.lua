0368d29f9f7104da09e1079877c824f356b6ad35
Victor Tereschenko <sibvic@gmail.com>
2023-12-26 18:36:24 +0600

    feat: Support str.format

--- snippets/Str.lua
+++ snippets/Str.lua
@@ -1,4 +1,59 @@
 Str = {};
+function Str:NewVar(value)
+    local var = {};
+    var.items = {};
+    var.items[0] = value;
+    function var:Get(period)
+        if period < 0 then
+            return nil;
+        end
+        return self.items[period];
+    end
+    function var:Set(period, value)
+        self.items[period] = value;
+    end
+    return var;
+end
+function Str:Clear()
+end
+function Str:doFormat(pattern, values)
+    local tokens = core.parseCsv(pattern, ",");
+    local value = values[tonumber(tokens[0])];
+    if value == nil then
+        return "";
+    end
+    if tokens[1] == "number" then
+        if tokens[2] == "percent" then
+            return tostring(math.floor(value + 0.5)) .. "%";
+        end
+    end
+    return tostring(value);
+end
+function Str:Format(pattern, value0, value1, value2, value3, value4, value5, value6, value7, value8, value9)
+    local values = {};
+    values[0] = value0;
+    values[1] = value1;
+    values[2] = value2;
+    values[3] = value3;
+    values[4] = value4;
+    values[5] = value5;
+    values[6] = value6;
+    values[7] = value7;
+    values[8] = value8;
+    values[9] = value9;
+
+    local tokens = core.parseCsv(pattern, "{");
+    local result = "";
+    for i, token in ipairs(tokens) do
+        local subtokens, c = core.parseCsv(token, "}");
+        if c == 1 then
+            result = result .. token;
+        else
+            result = result .. Str:doFormat(subtokens[0], values) .. subtokens[1];
+        end
+    end
+    return result;
+end
 function Str:ToString(value, pattern)
     if pattern == nil then
         return tostring(value);
@@ -34,4 +89,16 @@ function Str:ToString(value, pattern)
     end
     
     return string.format(luaPattern, value);
+end
+function SafeSetString(str, period, value)
+    if str == nil then
+        return;
+    end
+    str:Set(period, value);
+end
+function SafeGetString(str, period)
+    if str == nil then
+        return;
+    end
+    return str:Get(period);
 end
