305f454fc1e983c6b7d0864a71a81df9b75c3850
Victor Tereschenko <sibvic@gmail.com>
2023-12-27 13:54:20 +0600

    feat: Time-related functions

--- snippets/PineScriptUtils.lua
+++ snippets/PineScriptUtils.lua
@@ -1,3 +1,70 @@
+PineScriptUtils = {};
+function PineScriptUtils:TimeframeFromLength(length)
+    if length == "t" then
+        return "t1"
+    elseif length == "D" then
+        return "D1";
+    elseif length == "W" then
+        return "W1";
+    elseif length == "M" then
+        return "M1";
+    end
+    local length_number = tonumber(length);
+    if length_number < 3600 then
+        return "m" .. tostring(length_number / 60);
+    end
+    return "H" .. tostring(length_number / 3600)
+end
+function PineScriptUtils:ParseSession(session)
+    local session_info = {};
+    local _, _, from_hour, from_minute, to_hour, to_minute = string.find(session, "(%d%d)(%d%d)-(%d%d)(%d%d)");
+    session_info.from_hour = from_hour and tonumber(from_hour) or 0;
+    session_info.from_minute = from_minute and tonumber(from_minute) or 0;
+    session_info.from = (session_info.from_hour * 60.0 + session_info.from_minute) * 60.0;
+    session_info.to_hour = to_hour and tonumber(to_hour) or 23;
+    session_info.to_minute = to_minute and tonumber(to_minute) or 59;
+    session_info.to = (session_info.to_hour * 60.0 + session_info.to_minute) * 60.0;
+    function session_info:IsInRange(time)
+        time = math.floor(time * 86400 + 0.5);
+        if self.from < self.to then
+            return time >= self.from and time <= self.to;
+        end
+        if self.from > self.to then
+            return time > self.from or time < self.to;
+        end
+    
+        return time == self.from;
+    end
+    return session_info;
+end
+function PineScriptUtils:Time(period, timeframe_length, session, timezone)
+    local timeframe = PineScriptUtils:TimeframeFromLength(timeframe_length);
+    if PineScriptUtils.tradingWeekOffset == nil then
+        PineScriptUtils.tradingWeekOffset = core.host:execute("getTradingWeekOffset");
+        PineScriptUtils.tradingDayOffset = core.host:execute("getTradingDayOffset");
+    end
+    local s, e = core.getcandle(timeframe, instance.source:date(period), PineScriptUtils.tradingDayOffset, PineScriptUtils.tradingWeekOffset);
+    local session_info = PineScriptUtils:ParseSession(session);
+    if not session_info:IsInRange(s % 1) then
+        return nil;
+    end
+    
+    return s * 86400000;
+end
+function PineScriptUtils:WeekOfYear(time, timezone)
+    local time_ole = time / 86400000;
+    local date_table = core.dateToTable(time_ole)
+    date_table.month = 1;
+    date_table.day = 1;
+    date_table.hour = 0;
+    date_table.min = 0;
+    date_table.sec = 0;
+    local first_day_ole = core.tableToDate(date_table);
+    date_table = core.dateToTable(first_day_ole);
+    first_day_ole = first_day_ole - date_table.wday + 1;
+    return math.floor(time_ole - first_day_ole / 7);
+end
+
 function Timestamp(year, month, day, hour, minute, second, tz)
     local date = {};
     date.month = month;
