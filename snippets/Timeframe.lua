1faa83c8a379bb0c286dd94388845b956c6b6c3f
Victor Tereschenko <sibvic@gmail.com>
2024-11-21 05:26:34 +0600

    feat: Add timeframe:change and inseconds

--- snippets/Timeframe.lua
+++ snippets/Timeframe.lua
@@ -18,3 +18,66 @@ function Timeframe:Period()
     end
     return "0";
 end
+function Timeframe:InSeconds(period, source)
+    if timeframe == "M" then
+        return "M1";
+    elseif timeframe == "D" then
+        return "D1";
+    elseif timeframe == "t" then
+        return "t1";
+    else
+        local minutes = tonumber(timeframe);
+        if minutes == nil then
+            return nil;
+        end
+        return minutes * 60;
+    end
+    return nil;
+end
+function Timeframe:GetBarSize(timeframe)
+    if timeframe == "M" then
+        return "M1";
+    elseif timeframe == "D" then
+        return "D1";
+    elseif timeframe == "t" then
+        return "t1";
+    else
+        local minutes = tonumber(timeframe);
+        if minutes == 1 then
+            return "m1";
+        elseif minutes == 5 then
+            return "m5";
+        elseif minutes == 15 then
+            return "m15";
+        elseif minutes == 30 then
+            return "m30";
+        elseif minutes == 60 then
+            return "h1";
+        elseif minutes == 120 then
+            return "h2";
+        elseif minutes == 180 then
+            return "h3";
+        elseif minutes == 240 then
+            return "h4";
+        elseif minutes == 360 then
+            return "h6";
+        elseif minutes == 480 then
+            return "h8";
+        end
+    end
+    return nil;
+end
+function Timeframe:Change(timeframe, source, period)
+    if period <= 0 then
+        return false;
+    end
+    local barSize = Timeframe:GetBarSize(timeframe);
+    if barSize == nil then
+        return false;
+    end
+    tradingWeekOffset = core.host:execute("getTradingWeekOffset");
+    tradingDayOffset = core.host:execute("getTradingDayOffset");
+    local currentDate = core.getcandle(barSize, source:date(period), tradingDayOffset, tradingWeekOffset);
+    local prevDate = core.getcandle(barSize, source:date(period - 1), tradingDayOffset, tradingWeekOffset);
+    return currentDate ~= prevDate;
+end
