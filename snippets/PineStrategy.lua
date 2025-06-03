a8e62422d88642a3b1936d561fa988eb9818b84c
Victor Tereschenko <sibvic@gmail.com>
2025-06-03 21:09:05 +0600

    feat: Signals for PS strategy functions

--- snippets/PineStrategy.lua
+++ snippets/PineStrategy.lua
@@ -1,4 +1,18 @@
 PineStrategy = {};
+PineStrategy.streams = {};
+function PineStrategy:CreateEntrySignalV4(id)
+    local signal = {};
+    if self.streams["entry_signal" .. id] == nil then
+        self.streams["entry_signal" .. id] = instance:addStream("entry_signal" .. id, core.Line, "Entry Signal Entry " .. id, "Entry Signal " .. id, core.colors().Red, 0, 0);
+    end
+    signal.stream = self.streams["entry_signal" .. id];
+    function signal:Execute(period, id, long, qty, limit, stop, oca_name, oca_type, comment, when, alert_message)
+        if when then
+            self.stream[period] = long and 1 or -1;
+        end
+    end
+    return signal;
+end
 function PineStrategy:EntryV4(id, long, qty, limit, stop, oca_name, oca_type, comment, when, alert_message)
     if not when then
         return;
@@ -10,6 +24,19 @@ function PineStrategy:EntryV5(id, direction, qty, limit, stop, oca_name, oca_typ
         return;
     end
     core.host:trace(alert_message or id);
+end
+function PineStrategy:CreateCloseSignalV4(id)
+    local signal = {};
+    if self.streams["close_signal" .. id] == nil then
+        self.streams["close_signal" .. id] = instance:addStream("close_signal" .. id, core.Line, "Close Signal Entry " .. id, "Close Signal " .. id, core.colors().Blue, 0, 0);
+    end
+    signal.stream = self.streams["close_signal" .. id];
+    function signal:Execute(period, id, when, qty, qty_percent, comment, alert_message)
+        if when then
+            self.stream[period] = 1;
+        end
+    end
+    return signal;
 end
 function PineStrategy:CloseV4(id, when, qty, qty_percent, comment, alert_message)
     if not when then
