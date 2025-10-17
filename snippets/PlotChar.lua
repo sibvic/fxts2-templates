e607e209da14cbf57dcc5bd0250d8407f68b1ddd
Victor Tereschenko <sibvic@gmail.com>
2025-10-17 20:51:21 +0600

    feat: Plotchar base impl

---
+++ snippets/PlotChar.lua
@@ -0,0 +1,16 @@
+PlotChar = {};
+function PlotChar:SetValue(plot, period, source, value, text, label, location)
+    if not value then
+        plot:setNoData(period);
+        return;
+    end
+    if location == "abovebar" or location == "top" then
+        plot:set(period, source.high[period], text, label);
+        return;
+    end
+    if location == "belowbar" or location == "bottom" then
+        plot:set(period, source.low[period], text, label);
+        return;
+    end
+    plot:set(period, value, text, label);
+end
