1d17c832f63a723eb3c49a58be6adbfc524e959f
Victor Tereschenko <sibvic@gmail.com>
2025-11-19 11:09:07 +0600

    feat: return used value for plotshape

--- snippets/PlotShape.lua
+++ snippets/PlotShape.lua
@@ -3,15 +3,16 @@ function PlotShape:SetValue(plot, period, source, value, text, label, location, 
     local clr, transp = Graphics:SplitColorAndTransparency(color);
     if not value or transp == 100 or clr == nil then
         plot:setNoData(period);
-        return;
+        return nil;
     end
     if location == "abovebar" or location == "top" then
         plot:set(period, source.high[period], text, label, clr);
-        return;
+        return source.high[period];
     end
     if location == "belowbar" or location == "bottom" then
         plot:set(period, source.low[period], text, label, clr);
-        return;
+        return source.low[period];
     end
     plot:set(period, value, text, label, clr);
+    return value;
 end
