0f7655eb3d42c12868ceadd70f12ef4f65830d49
Victor Tereschenko <sibvic@gmail.com>
2025-12-11 07:58:24 +0600

    feat: fill now accepts values instead of streams

--- snippets/Fill.lua
+++ snippets/Fill.lua
@@ -1,10 +1,10 @@
 Fill = {};
 function Fill:SetValue(up_channel, dn_channel, value1, value2, mineColor, period)
-    if not value1:hasData(period) or not value2:hasData(period) or mineColor ~= true then
+    if value1 == nil or value2 == nil or mineColor ~= true then
         up_channel:setNoData(period);
         dn_channel:setNoData(period);
         return;
     end
-    up_channel[period] = math.max(value1[period], value2[period]);
-    dn_channel[period] = math.min(value1[period], value2[period]);
+    up_channel[period] = math.max(value1, value2);
+    dn_channel[period] = math.min(value1, value2);
 end
