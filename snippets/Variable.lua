16634f4f2cab29bc073a01bc1888963e31e4a695
Victor Tereschenko <sibvic@gmail.com>
2025-12-11 20:08:17 +0600

    feat: Support series for variables

--- snippets/Variable.lua
+++ snippets/Variable.lua
@@ -2,19 +2,37 @@ Variable = {};
 function Variable:Create()
     local var = {};
     var._init = false;
+    var._hist = {};
+    var._last_period = nil;
     function var:Clear()
         self._init = false;
         self._value = nil;
+        self._hist = {};
     end
-    function var:Get()
+    function var:Get(period, shift)
+        if (shift ~= nil) then
+            local target_period = period - shift;
+            local found_value = nil;
+            for k, v in pairs(self._hist) do
+                if k > target_period then
+                    return found_value;
+                end
+                found_value = v;
+            end
+            return found_value;
+        end
         return self._value;
     end
-    function var:Set(value)
+    function var:Set(period, value)
+        if (self._last_period ~= period and self._last_period ~= nil) then
+            self._hist[self._last_period] = self._value;
+        end
         self._value = value;
+        self._last_period = period;
         self._init = true;
     end
     function var:IsInitialized()
-        return self._value;
+        return self._init;
     end
     return var;
 end
