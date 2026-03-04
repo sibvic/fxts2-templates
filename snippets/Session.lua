b24259cf1cfed8cc5005f959326920c9d6197a1b
Victor Tereschenko <sibvic@gmail.com>
2026-03-04 21:45:33 +0600

    feat: Session:IsLastBarRegular

--- snippets/Session.lua
+++ snippets/Session.lua
@@ -10,3 +10,13 @@ function Session:IsFirstBarRegular(source, period)
     local prevDate = core.getcandle("D1", source:date(period - 1), tradingDayOffset, tradingWeekOffset);
     return currentDate ~= prevDate;
 end
+function Session:IsLastBarRegular(source, period)
+    if period <= 0 then
+        return false;
+    end
+    tradingWeekOffset = core.host:execute("getTradingWeekOffset");
+    tradingDayOffset = core.host:execute("getTradingDayOffset");
+    local _, dayEnd = core.getcandle("D1", source:date(period), tradingDayOffset, tradingWeekOffset);
+    local _, barEnd = core.getcandle(source:barSize(), source:date(period), tradingDayOffset, tradingWeekOffset);
+    return dayEnd == barEnd;
+end
